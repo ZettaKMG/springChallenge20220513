501977296138f354bb94580939e22fa0d416f1c9
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-17 14:16:14 +0900

    controller

--- src/main/java/com/choongang/spr/controller/ChallengeController.java
+++ src/main/java/com/choongang/spr/controller/ChallengeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.choongang.spr.domain.BoardDto;
 import com.choongang.spr.domain.ReplyDto;
@@ -26,14 +27,14 @@ public class ChallengeController {
 	
 	
 	// Board 관련 코드	
-	@GetMapping("board/list")
+	@GetMapping("/board/list")
 	public void listBoard(Model model) {
 		List<BoardDto> list = boardService.listBoard();
 		
 		model.addAttribute("boardList", list);
 	}
 	
-	@GetMapping("board/{id}")
+	@GetMapping("/board/{id}")
 	public String getBoard(@PathVariable("id") int id, Model model) {
 		BoardDto dto = boardService.getBoard(id);
 		List<ReplyDto> replyList = replyService.listReplyByBoardId(id);
@@ -44,45 +45,45 @@ public class ChallengeController {
 		return "/challenge/board/get";
 	}
 	
-	@PostMapping("board/modify")
-	public String modifyBoard(BoardDto board) {
+	@PostMapping("/board/modify")
+	public String modifyBoard(BoardDto board, RedirectAttributes rttr) {
 		boolean success = boardService.updateBoard(board);
 		
 		if (success) {
-			
+			rttr.addFlashAttribute("message", "게시글 수정 성공");
 		} else {
-			
+			rttr.addFlashAttribute("message", "게시글 수정 실패");
 		}
 		
 		return "redirect:/challenge/board/" + board.getId();
 	}
 	
-	@PostMapping("board/remove")
-	public String removeBoard(int id) {
+	@PostMapping("/board/remove")
+	public String removeBoard(int id, RedirectAttributes rttr) {
 		boolean success = boardService.removeBoardById(id);
 		
 		if (success) {
-			
+			rttr.addFlashAttribute("message", "게시물 삭제 성공");
 		} else {
-			
+			rttr.addFlashAttribute("message", "게시물 삭제 실패");
 		}
 		
 		return "redirect:/challenge/board/list";
 	}
 	
-	@GetMapping("board/write")
+	@GetMapping("/board/write")
 	public void writeBoard() {
 		
 	}
 	
-	@PostMapping("board/write")
-	public String writeBoardProcess(BoardDto board) {
+	@PostMapping("/board/write")
+	public String writeBoardProcess(BoardDto board, RedirectAttributes rttr) {
 		boolean success = boardService.addBoard(board);
 		
 		if (success) {
-			
+			rttr.addFlashAttribute("message", "게시글 등록 성공");
 		} else {
-			
+			rttr.addFlashAttribute("message", "게시글 등록 실패");
 		}
 		
 		return "redirect:/challenge/board/" + board.getId();
@@ -90,39 +91,39 @@ public class ChallengeController {
 	
 	
 	// Reply 관련 코드
-	@PostMapping("reply/add")
-	public String addReply(ReplyDto reply) {
+	@PostMapping("/reply/add")
+	public String addReply(ReplyDto reply, RedirectAttributes rttr) {
 		boolean success = replyService.addReply(reply);
 		
 		if (success) {
-			
+			rttr.addFlashAttribute("message", "댓글 등록 성공");
 		} else {
-			
+			rttr.addFlashAttribute("message", "댓글 등록 실패");
 		}
 		
 		return "redirect:/challenge/board/" + reply.getBoardId();
 	}
 	
-	@PostMapping("reply/remove")
-	public String removeReply(ReplyDto reply) {
+	@PostMapping("/reply/remove")
+	public String removeReply(ReplyDto reply, RedirectAttributes rttr) {
 		boolean success = boardService.removeReplyById(reply.getId());
 		
 		if (success) {
-			
+			rttr.addFlashAttribute("message", "댓글 삭제 성공");
 		} else {
-			
+			rttr.addFlashAttribute("message", "댓글 삭제 실패");
 		}
 		
 		return "redirect:/challenge/board/" + reply.getBoardId();
 	}
 	
-	public String modifyReply(ReplyDto reply) {
+	public String modifyReply(ReplyDto reply, RedirectAttributes rttr) {
 		boolean success = boardService.modifyReply(reply);
 		
 		if (success) {
-			
+			rttr.addFlashAttribute("message", "댓글 수정 성공");
 		} else {
-			
+			rttr.addFlashAttribute("message", "댓글 수정 실패");
 		}
 		
 		return "redirect:/challenge/board/" + reply.getBoardId();
