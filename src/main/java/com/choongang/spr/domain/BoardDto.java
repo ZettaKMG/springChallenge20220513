403e78e5843e802042b122c43a0f5ff9f7f810b8
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-17 10:15:08 +0900

    JavaBeans

---
+++ src/main/java/com/choongang/spr/domain/BoardDto.java
@@ -0,0 +1,14 @@
+package com.choongang.spr.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class BoardDto {
+	private int id;
+	private String title;
+	private String body;
+	private LocalDateTime inserted;
+	private int numOfReply;
+}
