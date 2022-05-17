76a8ad7a88f6193a888c943d2c290f149e39a787
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-05-17 12:21:35 +0900

    service

--- src/main/java/com/choongang/spr/service/ChallengeService.java
+++ src/main/java/com/choongang/spr/service/ChallengeService.java
@@ -4,19 +4,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.choongang.spr.domain.BoardDto;
+import com.choongang.spr.domain.ReplyDto;
 import com.choongang.spr.mapper.Mapper;
 
 @Service
-public class Service {
+public class ChallengeService {
 	// Board, Reply Mapper 불러오기
 	@Autowired
 	private Mapper boardMapper;
 	
 	@Autowired
 	private Mapper replyMapper;
+	
 	
 	// Board 관련 코드	
 	public List<BoardDto> listBoard(){
@@ -33,7 +36,6 @@ public class Service {
 		return count == 1;
 	}
 	
-	// Reply 관련 코드(Board 관련 코드 일부 포함)
 	@Transactional
 	public boolean removeBoardById(int id) {
 		replyMapper.deleteReplyByBoard(id);
@@ -50,4 +52,33 @@ public class Service {
 		
 		return count == 1;
 	}
+	
+	
+	// Reply 관련 코드
+	public List<ReplyDto> listReplyByBoardId(int id) {
+		return replyMapper.selectReplyByBoardId(id);
+	}
+	
+	public boolean addReply(ReplyDto reply) {
+		reply.setInserted(LocalDateTime.now());
+		
+		int count = replyMapper.insertReply(reply);
+		
+		return count == 1;
+	}
+
+	public boolean modifyReply(ReplyDto reply) {
+		int count = replyMapper.updateReply(reply);
+		
+		return count == 1;
+	}
+
+	public boolean removeReplyById(int id) {
+		replyMapper.deleteReplyById(id);
+		
+		int count = replyMapper.deleteReplyById(id);
+		
+		return count == 1;
+	}
+
 }
